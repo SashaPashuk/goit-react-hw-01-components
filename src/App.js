256300b9dcd0203578c4cfaf503cb09c084ea4cd
Alexander <pashuk.sasha18@gmail.com>
2021-02-21 10:27:05 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react"
+import Profile from "./components/ProfileUser/Profile"
+import user from "./base/profile.json"
+import Statistics from "./components/Statistics/Statistics"
+import StatisticsBase from "./base/statistics.json"
+import FriendList from "./components/Friends/Friends"
+import friends from "./base/friends.json";
+
+const App  = () => {
+  return ( 
+    <>
+<Profile
+  name={user.name}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
+
+   <Statistics items={StatisticsBase}/>,
+   <FriendList friends={friends} />,
+ </>
+ )
+}
+export default App
+
+// ======TASK2=====
+
+
---
+++ src/components/Friends/Friends.js
@@ -0,0 +1,22 @@
+import React from "react"
+import s from "./Friends.module.scss"
+
+const FriendListItem = ({ avatar, name, isOnline,  id  }) => {
+    return (
+        <li key={id} className={s.item}>
+  <span className={s.status}>{isOnline}</span>
+  <img className={s.avatar} src={avatar} alt={name} width="48" />
+  <p className={s.name}>{name}</p>
+</li>
+    )
+}
+
+
+const FriendList = ({ friends }) => {
+    return (
+        <ul className={s.friendList}>
+        {friends.map(FriendListItem)} </ul>
+    )
+}   
+
+export default FriendList
